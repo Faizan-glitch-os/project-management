7e83d98f473774983a242db04f0c4d8da510579d
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-04-18 07:54:50 +0500

    view single project designed, functionality still remains

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ function App() {
             <h2 className="text-3xl font-bold text-neutral-600">
               {addProjects[0].title}
             </h2>
-            <button className="text-2xl   text-neutral-600 hover:text-neutral-800 rounded-xl">
+            <button className="text-xl   text-neutral-600 hover:text-neutral-800 rounded-xl">
               Delete
             </button>
           </div>
@@ -67,6 +67,28 @@ function App() {
           <p className="text-lg font-semibold text-neutral-600">
             {addProjects[0].description}
           </p>
+          <hr className="border-t-2 border-neutral-500 mb-4" />
+          <h2 className="text-3xl font-bold text-neutral-600">Tasks</h2>
+          <p className="flex  gap-4">
+            <input
+              type="text"
+              id="title"
+              required
+              placeholder="add task"
+              className={`h-16 resize-none overflow-hidden bg-neutral-200 rounded-md w-2/5 text-2xl text-neutral-800 px-2  border-b-neutral-400 border-b-4 focus:border-b-neutral-600 border-transparent focus:outline-none`}
+            />
+            <button className="text-xl   text-neutral-600 hover:text-neutral-800 rounded-xl">
+              Add Task
+            </button>
+          </p>
+          <ol className="flex flex-col  bg-neutral-200 rounded-sm px-4 py-8 mt-4 gap-6">
+            <p className="flex justify-between  ">
+              <li className="text-xl text-neutral-800">1st item</li>
+              <button className="text-xl   text-neutral-600 hover:text-neutral-800 rounded-xl">
+                Clear
+              </button>
+            </p>
+          </ol>
         </div>
       </section>
     </>
